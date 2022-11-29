68fddd3a9a2a65a10a1e6a8410138d621180f41e
HelenFedoseeva <101935002+HelenFedoseeva@users.noreply.github.com>
2022-11-29 14:53:58 +0000

    Deploying to gh-pages from @ HelenFedoseeva/UWinnipegAlumni@00672bad8e2d44e1e4ce3cc1ef21a036f5a4652b 🚀

---
+++ about.c5a8f150.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("#modal"),t=document.querySelector(".backdrop");function d(e){t.classList.add("is-hidden")}e.addEventListener("click",(function(e){e.preventDefault(),t.classList.remove("is-hidden"),document.getElementById("#modal-close").addEventListener("click",d)}))}();
+//# sourceMappingURL=about.c5a8f150.js.map
