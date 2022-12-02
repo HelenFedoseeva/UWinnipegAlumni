8463f4da685205d845b665a500972a90f6a9d92b
HelenFedoseeva <101935002+HelenFedoseeva@users.noreply.github.com>
2022-12-02 12:10:11 +0000

    Deploying to gh-pages from @ HelenFedoseeva/UWinnipegAlumni@988424e2ed1a555a4a833153bf478f7626cb01fc 🚀

--- about.fafec2ca.js
+++
@@ -1,2 +0,0 @@
-const e=document.getElementById("#modal"),t=document.querySelector(".backdrop");function d(e){t.classList.add("is-hidden")}e.addEventListener("click",(function(e){e.preventDefault(),t.classList.remove("is-hidden");document.getElementById("#modal-close").addEventListener("click",d)}));
-//# sourceMappingURL=about.fafec2ca.js.map
