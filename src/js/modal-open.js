988424e2ed1a555a4a833153bf478f7626cb01fc
Helen <helenlena52@gmail.com>
2022-12-02 14:09:42 +0200

    add-modal-to-all-pages

--- src/js/modal-open.js
+++ src/js/modal-open.js
@@ -2,6 +2,10 @@ const btnRef = document.getElementById('#modal');
 const backdropRef = document.querySelector('.backdrop');
 
 btnRef.addEventListener('click', onClickHadler);
+// Close modal to Escape
+document.addEventListener('keydown', closeModalEscape);
+//Сlose modal on click outside
+document.addEventListener('click', closeModalClikcOutside);
 
 function onClickHadler(e) {
   e.preventDefault();
@@ -9,9 +13,32 @@ function onClickHadler(e) {
   backdropRef.classList.remove('is-hidden');
 
   const closeIconRef = document.getElementById('#modal-close');
+  const modalFormRef = document.querySelector('.modal__form');
+
   closeIconRef.addEventListener('click', onCloseBtnClick);
+
+  modalFormRef.addEventListener('click', event => {
+    if (event.target.nodeName !== 'BUTTON') {
+      return;
+    }
+    onCloseBtnClick();
+  });
 }
 
 function onCloseBtnClick(e) {
   backdropRef.classList.add('is-hidden');
+}
+
+function closeModalEscape(e) {
+  if (e.key === 'Escape') {
+    onCloseBtnClick();
+  }
+}
+
+function closeModalClikcOutside(e) {
+  console.log(e.target.classList.value);
+  if (e.target.classList.value !== 'backdrop') {
+    return;
+  }
+  onCloseBtnClick();
 }
